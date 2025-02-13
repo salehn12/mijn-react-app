c5c385413a95c73cdc63c50ccf067a9fe3648cc2
salehn12 <6047299@mborijnland.nl>
2025-02-13 18:05:50 +0100

    initialize React app with essential files and configurations

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((json) => setData(json.message));
+  }, []);
+
+  return <h1>{data || "Laden..."}</h1>;
+}
+
+export default App;
